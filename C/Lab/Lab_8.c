b196975882191a6c8a2f5b4f534916a9dbc66c39
EmreAlaybeyoglu <89883546+EmreAlaybeyoglu@users.noreply.github.com>
2022-12-09 00:26:26 +0300

    Update Lab_8.c

--- C/Lab/Lab_8.c
+++ C/Lab/Lab_8.c
@@ -3,13 +3,13 @@
 int main()
 {
 	int no;
-	int visa;
+	int midterm;
 	int final;
 	int note;
 	scanf_s("%d", &no);
-	scanf_s("%d", &visa);
+	scanf_s("%d", &midterm);
 	scanf_s("%d", &final);
-	note = (visa * 0.4) + (final * 0.6);
+	note = (midterm* 0.4) + (final * 0.6);
 	printf("%d", no);
 	printf("\n%d", note);
 		
